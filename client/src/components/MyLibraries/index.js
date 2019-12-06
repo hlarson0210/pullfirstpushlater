fba095a24e32f189e6bcf2ae82c7ec501ff8207c
Cuong XPS <nguyenc72694@gmail.com>
2019-12-05 19:57:29 -0600

    added fonts and footer

--- client/src/components/MyLibraries/index.js
+++ client/src/components/MyLibraries/index.js
@@ -1,11 +1,58 @@
 import React from "react";
 import "./style.css";
 
-function MyLibraries () {
-    return(
+function MyLibraries() {
+    return (
         <main>
             <div className="container center">
-                <h1>My Libraries</h1>
+                <h1>My Library</h1>
+
+                <div className="row">
+                    <div className="col s4">
+                        <div className="card horizontal">
+                            <div className="card-image">
+                                <img src="https://lorempixel.com/100/190/nature/6" alt="something"></img>
+                            </div>
+                            <div className="card-stacked">
+                                <div className="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information.</p>
+                                </div>
+                                <div className="card-action">
+                                    <a href="https://www.youtube.com/">This is a link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col s4">
+                        <div className="card horizontal">
+                            <div className="card-image">
+                                <img src="https://lorempixel.com/100/190/nature/6" alt="something"></img>
+                            </div>
+                            <div className="card-stacked">
+                                <div className="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information.</p>
+                                </div>
+                                <div className="card-action">
+                                    <a href="https://www.youtube.com/">This is a link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div><div className="col s4">
+                        <div className="card horizontal">
+                            <div className="card-image">
+                                <img src="https://lorempixel.com/100/190/nature/6" alt="something"></img>
+                            </div>
+                            <div className="card-stacked">
+                                <div className="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information.</p>
+                                </div>
+                                <div className="card-action">
+                                    <a href="https://www.youtube.com/">This is a link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     )
