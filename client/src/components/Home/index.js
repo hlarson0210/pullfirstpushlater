b0e5277b8ec4cc13544e61a2568b4c4aae3ae5b4
Cuong XPS <nguyenc72694@gmail.com>
2019-12-10 10:51:34 -0600

    fixed the loading issues on the components we had

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,131 +1,140 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-// import './style.css'
+import React from "react"
+import { Link } from "react-router-dom"
+// import "./style.css"
+// Import Materialize
+import M from "materialize-css";
 
-function Home () {
-  return (
-    <main>
-      <div class='slider'>
-        <ul class='slides'>
-          <li>
-            <img
-              src='https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/11/25/1416910214392/51b84e46-e586-4b72-a25c-2bf7703b5b66-2060x1236.jpeg'
-              style={{ opacity: 0.3 }}
-            />
-            <div class='caption center-align'>
-              <h1>FAST PLAY</h1>
-              <h3 class='light grey-text text-lighten-3'>
-                Find your board game fast.
-              </h3>
-            </div>
-          </li>
-          <li>
-            <img src='https://lorempixel.com/580/250/nature/2' />
-            <div class='caption left-align'>
-              <h3>MY GAME LIBRARIES</h3>
-              <h5 class='light grey-text text-lighten-3'>
-                Create a searchable library of all your board game options.
-              </h5>
-            </div>
-          </li>
-          <li>
-            <img src='https://lorempixel.com/580/250/nature/3' />
-            <div class='caption right-align'>
-              <h3>EXPLORE GAMES</h3>
-              <h5 class='light grey-text text-lighten-3'>
-                Find new games to purchase and add to your library.
-              </h5>
-            </div>
-          </li>
-          <li>
-            <img src='https://lorempixel.com/580/250/nature/4' />
-            <div class='caption center-align'>
-              <h3>Life's short. Play fast.</h3>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div className='container center instructions'>
-        <br />
-        <br />
-        <h4>How it Works</h4>
-        <i class='material-icons'>expand_more</i>
-      </div>
-    </main>
-  )
+class Home extends React.Component {
+  componentDidMount() {
+    // Auto initialize all the things!
+    M.AutoInit();
+  }
+  render() {
+    return (
+      <main>
+        <div class="slider">
+          <ul class="slides">
+            <li>
+              <img
+                src="https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/11/25/1416910214392/51b84e46-e586-4b72-a25c-2bf7703b5b66-2060x1236.jpeg"
+                style={{ opacity: 0.3 }}
+              />
+              <div class="caption center-align">
+                <h1>FAST PLAY</h1>
+                <h3 class="light grey-text text-lighten-3">
+                  Find your board game fast.
+                </h3>
+              </div>
+            </li>
+            <li>
+              <img src="https://lorempixel.com/580/250/nature/2" />
+              <div class="caption left-align">
+                <h3>MY GAME LIBRARIES</h3>
+                <h5 class="light grey-text text-lighten-3">
+                  Create a searchable library of all your board game options.
+                </h5>
+              </div>
+            </li>
+            <li>
+              <img src="https://lorempixel.com/580/250/nature/3" />
+              <div class="caption right-align">
+                <h3>EXPLORE GAMES</h3>
+                <h5 class="light grey-text text-lighten-3">
+                  Find new games to purchase and add to your library.
+                </h5>
+              </div>
+            </li>
+            <li>
+              <img src="https://lorempixel.com/580/250/nature/4" />
+              <div class="caption center-align">
+                <h3>Life"s short. Play fast.</h3>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="container center instructions">
+          <br />
+          <br />
+          <h4>How it Works</h4>
+          <i class="material-icons">expand_more</i>
+        </div>
+      </main>
+    )
+  }
 }
+
 export default Home
 
 //  EFF THIS FOR NOW WE NEED MVP FOR TOMORROW AND THIS IS SHIT...reverting back to first slider concept for mvp presentation
-/* import React from 'react'
-// import './style.scss'
-import './style.css'
+/* import React from "react"
+// import "./style.scss"
+import "./style.css"
 
 function Home () {
   return (
-    <div className='contenedor'>
-      <form className='vert-nav-form'>
+    <div className="contenedor">
+      <form className="vert-nav-form">
         <input
-          type='radio'
-          className='radio-btn'
-          id='Slide1'
-          name='slider'
-          titulo='Home'
-          autoFocus='autofocus'
+          type="radio"
+          className="radio-btn"
+          id="Slide1"
+          name="slider"
+          titulo="Home"
+          autoFocus="autofocus"
         />
         <input
-          type='radio'
-          className='radio-btn'
-          id='Slide2'
-          name='slider'
-          titulo='MyLibraries'
+          type="radio"
+          className="radio-btn"
+          id="Slide2"
+          name="slider"
+          titulo="MyLibraries"
         />
         <input
-          type='radio'
-          className='radio-btn'
-          id='Slide3'
-          name='slider'
-          titulo='ExploreGames'
+          type="radio"
+          className="radio-btn"
+          id="Slide3"
+          name="slider"
+          titulo="ExploreGames"
         />
-        <div className='labels'>
-          <label className='Slide' htmlFor='Slide1' id='Slide1'>
-            <div className='content'>
+        <div className="labels">
+          <label className="Slide" htmlFor="Slide1" id="Slide1">
+            <div className="content">
               <h1>
                 <strong>GAME APP</strong>
               </h1>
             </div>
-            <div className='icon'>
+            <div className="icon">
               <span />
               <span />
             </div>
           </label>
-          <label className='Slide' htmlFor='Slide2' id='Slide2'>
-            <div className='content'>
+          <label className="Slide" htmlFor="Slide2" id="Slide2">
+            <div className="content">
               <h1>MY GAME LIBRARIES</h1>
-              <div className='block'>
+              <div className="block">
                 <span>
-                  {' '}
-                  <i className='fa fa-keyboard-o' />
+                  {" "}
+                  <i className="fa fa-keyboard-o" />
                   <br />
                   <strong>No JS</strong>
                   <br />
                   CSS Rules!
                 </span>
                 <span>
-                  {' '}
-                  <i className='fa fa-keyboard-o' />
+                  {" "}
+                  <i className="fa fa-keyboard-o" />
                   <br />
                   <strong>Keyboard navigation</strong>
                   <br />
                   Yep, no kidding
                 </span>
                 <span>
-                  <i className='fa fa-bars' />
+                  <i className="fa fa-bars" />
                   <br />
                   <strong>Lateral menu</strong>
                 </span>
                 <span>
-                  <i className='fa fa-codepen' />
+                  <i className="fa fa-codepen" />
                   <br />
                   <strong>Autogenerate pages</strong>
                   <br />
@@ -134,10 +143,10 @@ function Home () {
               </div>
             </div>
           </label>
-          <label className='Slide' htmlFor='Slide3' id='Slide3'>
-            <div className='content'>
+          <label className="Slide" htmlFor="Slide3" id="Slide3">
+            <div className="content">
               <h1>EXPLORE GAMES</h1>
-              <div className='block'>
+              <div className="block">
                 <ol>
                   <li>
                     Add the pages title in the pageTitle array in the HTML
