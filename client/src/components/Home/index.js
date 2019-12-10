c8f8c55743888e0f8f1f959b7e1738f7b5bcbde5
Nicole Marty <nmarty310@gmail.com>
2019-12-10 11:03:41 -0600

    all changes to homepage design. Still in progress, but this is the overall concept

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,67 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-// import './style.css'
-
-function Home () {
-  return (
-    <main>
-      <div class='slider'>
-        <ul class='slides'>
-          <li>
-            <img
-              src='https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/11/25/1416910214392/51b84e46-e586-4b72-a25c-2bf7703b5b66-2060x1236.jpeg'
-              style={{ opacity: 0.3 }}
-            />
-            <div class='caption center-align'>
-              <h1>FAST PLAY</h1>
-              <h3 class='light grey-text text-lighten-3'>
-                Find your board game fast.
-              </h3>
-            </div>
-          </li>
-          <li>
-            <img src='https://lorempixel.com/580/250/nature/2' />
-            <div class='caption left-align'>
-              <h3>MY GAME LIBRARIES</h3>
-              <h5 class='light grey-text text-lighten-3'>
-                Create a searchable library of all your board game options.
-              </h5>
-            </div>
-          </li>
-          <li>
-            <img src='https://lorempixel.com/580/250/nature/3' />
-            <div class='caption right-align'>
-              <h3>EXPLORE GAMES</h3>
-              <h5 class='light grey-text text-lighten-3'>
-                Find new games to purchase and add to your library.
-              </h5>
-            </div>
-          </li>
-          <li>
-            <img src='https://lorempixel.com/580/250/nature/4' />
-            <div class='caption center-align'>
-              <h3>Life's short. Play fast.</h3>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div className='container center instructions'>
-        <br />
-        <br />
-        <h4>How it Works</h4>
-        <i class='material-icons'>expand_more</i>
-      </div>
-    </main>
-  )
-}
-export default Home
-
-//  EFF THIS FOR NOW WE NEED MVP FOR TOMORROW AND THIS IS SHIT...reverting back to first slider concept for mvp presentation
-/* import React from 'react'
-// import './style.scss'
+import SignIn from '../SignIn'
 import './style.css'
 
-function Home () {
+/* class Home extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selectedOption: 'Home'
+    }
+
+    handleOptionChange = changeEvent => {
+      this.setState({
+        selectedOption: changeEvent.target.value
+      })
+    }
+  }
+  render () {
+    return (
+      <div className='contenedor'>
+        <form>
+          <div className='vert-nav-form'>
+            <label>
+              <input
+                type='radio'
+                name='slider'
+                value='Home'
+                checked={this.state.selectedOption === 'Home'}
+                onChange={this.handleOptionChange}
+                className='radio-btn'
+              />
+              Home
+            </label>
+          </div>
+          <div className='vert-nav-form'>
+            <label>
+              <input
+                type='radio'
+                name='slider'
+                value='MyLibraries'
+                checked={this.state.selectedOption === 'MyLibraries'}
+                onChange={this.handleOptionChange}
+                className='radio-btn'
+              />
+              My Libraries
+            </label>
+          </div>
+          <div className='vert-nav-form'>
+            <label>
+              <input
+                type='radio'
+                name='slider'
+                value='ExploreGames'
+                checked={this.state.selectedOption === 'ExploreGames'}
+                onChange={this.handleOptionChange}
+                className='radio-btn'
+              />
+              Explore Games
+            </label>
+          </div>
+          <div className='vert-nav-form'>
+            <label>
+              <input
+                type='radio'
+                name='slider'
+                value='HowItWorks'
+                checked={this.state.selectedOption === 'HowItWorks'}
+                onChange={this.handleOptionChange}
+                className='radio-btn'
+              />
+              How It Works
+            </label>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+export default Home */
+
+function Home (props) {
   return (
     <div className='contenedor'>
       <form className='vert-nav-form'>
@@ -87,11 +105,19 @@ function Home () {
           name='slider'
           titulo='ExploreGames'
         />
+        <input
+          type='radio'
+          className='radio-btn'
+          id='Slide4'
+          name='slider'
+          titulo='HowItWorks'
+        />
         <div className='labels'>
           <label className='Slide' htmlFor='Slide1' id='Slide1'>
             <div className='content'>
               <h1>
-                <strong>GAME APP</strong>
+                <strong />
+                <SignIn />
               </h1>
             </div>
             <div className='icon'>
@@ -101,53 +127,38 @@ function Home () {
           </label>
           <label className='Slide' htmlFor='Slide2' id='Slide2'>
             <div className='content'>
-              <h1>MY GAME LIBRARIES</h1>
-              <div className='block'>
-                <span>
-                  {' '}
-                  <i className='fa fa-keyboard-o' />
-                  <br />
-                  <strong>No JS</strong>
-                  <br />
-                  CSS Rules!
-                </span>
-                <span>
-                  {' '}
-                  <i className='fa fa-keyboard-o' />
-                  <br />
-                  <strong>Keyboard navigation</strong>
-                  <br />
-                  Yep, no kidding
-                </span>
-                <span>
-                  <i className='fa fa-bars' />
-                  <br />
-                  <strong>Lateral menu</strong>
-                </span>
-                <span>
-                  <i className='fa fa-codepen' />
-                  <br />
-                  <strong>Autogenerate pages</strong>
-                  <br />
-                  Using Jade & SASS
-                </span>
-              </div>
+              <h1 className='my-libraries-title'>MY LIBRARIES</h1>
+              <p>
+                <a href='./mylibraries'>take me there.</a>
+              </p>
             </div>
           </label>
           <label className='Slide' htmlFor='Slide3' id='Slide3'>
             <div className='content'>
-              <h1>EXPLORE GAMES</h1>
-              <div className='block'>
-                <ol>
-                  <li>
-                    Add the pages title in the pageTitle array in the HTML
-                    editor to generate pages
-                  </li>
-                  <li>
-                    Add the number of pages in the $npages variable in the CSS
-                    editor
-                  </li>
-                </ol>
+              <h1 className='explore-games-title'>EXPLORE GAMES</h1>
+              <p>
+                <a href='./exploregames'>take me there.</a>
+              </p>
+            </div>
+          </label>
+          <label className='Slide' htmlFor='Slide4' id='Slide4'>
+            <div className='content'>
+              <h1 className='how-it-works-title'>HOW IT WORKS</h1>
+              <div className='row'>
+                <div className='col s12 m6 l6'>
+                  <div className='card'>
+                    <div className='card-content'>
+                      <div className='card-title'>My Libraries</div>
+                    </div>
+                  </div>
+                </div>
+                <div className='col s12 m6 l6'>
+                  <div className='card'>
+                    <div className='card-content'>
+                      <div className='card-title'>Explore Games</div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </label>
@@ -157,4 +168,4 @@ function Home () {
   )
 }
 
-export default Home */
+export default Home
--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -1,74 +1,73 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./style.css";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.css'
 
-function SideNav(props) {
+function SideNav (props) {
   return (
     <header>
-      <ul id="slide-out" className="sidenav blue lighten-1">
+      <ul id='slide-out' className='sidenav blue lighten-1'>
         <li>
-          <div className="user=view">
-            <a href="#user"><img className="circle" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"></img></a>
-            <a href="#name"><span className="name white-text">John Doe</span></a>
+          <div className='user=view'>
+            <a href='#user'>
+              <img
+                className='circle'
+                src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+              />
+            </a>
+            <a href='#name'>
+              <span className='name white-text'>John Doe</span>
+            </a>
           </div>
         </li>
         <li>
-          <div className="divider"></div>
+          <div className='divider' />
         </li>
-        <li><a className="subheader">Subheader</a></li>
-        <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/signin"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Sign In
-            </Link>
+        <li>
+          <a className='subheader'>Subheader</a>
         </li>
-        <li className="nav-item">
-            <Link
-              to="/home"
-              className={
-                window.location.pathname === "/home"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Home
-            </Link>
+        <li className='nav-item'>
+          <Link
+            to='/home'
+            className={
+              window.location.pathname === '/' ||
+              window.location.pathname === '/home'
+                ? 'nav-link active'
+                : 'nav-link'
+            }
+          >
+            Home
+          </Link>
         </li>
-        <li className="nav-item">
-            <Link
-              to="/mylibraries"
-              className={
-                window.location.pathname === "/mylibraries"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              My Libraries
-            </Link>
+        <li className='nav-item'>
+          <Link
+            to='/mylibraries'
+            className={
+              window.location.pathname === '/mylibraries'
+                ? 'nav-link active'
+                : 'nav-link'
+            }
+          >
+            My Libraries
+          </Link>
         </li>
-        <li className="nav-item">
-            <Link
-              to="/exploregames"
-              className={
-                window.location.pathname === "/exploregames"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Explore Games
-            </Link>
+        <li className='nav-item'>
+          <Link
+            to='/exploregames'
+            className={
+              window.location.pathname === '/exploregames'
+                ? 'nav-link active'
+                : 'nav-link'
+            }
+          >
+            Explore Games
+          </Link>
         </li>
-
       </ul>
-      <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons small">menu</i></a>
+      <a href='#' data-target='slide-out' className='sidenav-trigger'>
+        <i className='material-icons small menu'>menu</i>
+      </a>
     </header>
-  );
+  )
 }
 
-export default SideNav;
+export default SideNav
