314c93bffa770aca62ad604275a7419f9eeb26e8
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-12-15 13:37:14 -0600

    final css tweaks for the day

--- client/src/pages/GameCard.js
+++ client/src/pages/GameCard.js
@@ -1,17 +1,17 @@
 import React from 'react'
 let style = {
-  marginRight: '2%'
+  marginRight: '8px'
 }
 
 function GameCard (props) {
   return (
     <div className='row'>
       <div className='right col s12'>
-        <li className='collection-item hoverable'>
+        <li className='collection-item hoverable popularGameLI'>
           <div>
             <a href={props.officialsite} target='_blank'>
               <img
-                className='left valign-wrapper'
+                className='left valign-wrapper popularGameImg'
                 style={style}
                 src={props.image}
                 alt=""
